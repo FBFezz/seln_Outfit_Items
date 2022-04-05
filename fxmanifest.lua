96ab225a5d0e5a6b74546c8f79e601c3e118c1a6
Fezz <43589668+FBFezz@users.noreply.github.com>
2022-04-05 16:03:02 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,12 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'bodacious' 
+game 'gta5'
 
 server_scripts {
  	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
 	'Server.lua',
-
- }
+}
  
-client_scripts {
- 	'Client.lua',
- }
+client_script 'Client.lua'
 
+dependency 'es_extended'
